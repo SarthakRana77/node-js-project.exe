4ea949528994c112cbbc9f8e5e7109edcecb647d
SarthakRana77 <sarthak.rana7895@gmail.com>
2022-10-19 13:24:58 +0530

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://sarthak:SarthakRana@sarthak.wglmfsr.mongodb.net/shop'; 
+  'mongodb+srv://sarthak:SarthakRana@sarthak.wglmfsr.mongodb.net/shop';
 
 const app = express();
 const store = new MongoDBStore({
